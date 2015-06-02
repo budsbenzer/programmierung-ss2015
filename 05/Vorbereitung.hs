8a55daef527bf4bc7dbac7d7c7b4f7dc1b587d2e
Johannes Osterholzer <Johannes.Osterholzer@tu-dresden.de>
2015-06-02 08:51:05 +0200

    Fixup: Tutorial 05, not 06.

--- 05/Vorbereitung.hs
+++ 05/Vorbereitung.hs
@@ -2,6 +2,41 @@ module Vorbereitung where
 
 import Prelude hiding (foldl)
 
+
+-- Uebung 1
+
+data Tree a = Node a (Tree a) (Tree a) | Leaf a deriving Show
+
+class Zippable f where
+  genericZip     :: f a -> f b -> f (a, b)
+  genericZipWith :: (a -> b -> c) -> f a -> f b -> f c
+
+  -- Standardimplementierung von genericZip durch genericZipWith
+  genericZip = genericZipWith (,)
+  -- Wäre f zusätzlich Instanz von Funktor, dann ginge es auch anders rum:
+  --   genericZipWith f u v = fmap (uncurry f) (genericZip u v)
+
+
+instance Zippable Tree where
+  genericZip (Leaf a) (Leaf b) = Leaf (a, b)
+  genericZip (Leaf a) (Node b _ _) = Leaf (a, b)
+  genericZip (Node a s t) (Node b u v) = Node (a, b) (genericZip s u) (genericZip t v)
+
+  genericZipWith f (Leaf a) (Leaf b) = Leaf (f a b)
+  genericZipWith f (Leaf a) (Node b _ _) = Leaf (f a b)
+  genericZipWith f (Node a s t) (Node b u v)
+    = Node (f a b) (genericZipWith f s u) (genericZipWith f t v)
+
+
+data RoseTree a = RNode a [RoseTree a] deriving Show
+
+instance Zippable RoseTree where
+  genericZipWith f (RNode a ls) (RNode b rs)
+    = RNode (f a b) (zipWith (genericZipWith f) ls rs)
+
+
+-- Uebung 2
+
 foldl :: (b -> a -> b) -> b -> [a] -> b
 foldl f z [] = z
 foldl f z (x:xs) = foldl f (f z x) xs
